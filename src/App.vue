24f3d8f4adda4655f2387712ad416cbc3f24a8f1
Nazar Brunarskyi <3nnazar836@gmail.com>
2023-02-24 23:58:07 +0200

    rate board is finished

--- src/App.vue
+++ src/App.vue
@@ -2,34 +2,22 @@
 import { defineComponent } from 'vue';
 import { getMoney } from './API/getData'
 import CurrencyRates from './components/Currency-rates.vue'
+import CurrencyConverter from './components/Currency-converter.vue'
 import type { Rates } from './types/ratesType'
 
 interface State {
   rates: Rates | null;
-  currentRateOfCurrencyToConvert: number,
-  currentRateOfConvertedCurrency: number,
-  currencyToConvert: string,
-  convertedCurrency: string,
-  amountOfCurrencyToConvert: number,
-  amountOfConvertedCurrency: number,
-  conversionLimit: number,
 }
 
 export default defineComponent({
   components: {
     CurrencyRates,
+    CurrencyConverter,
   },
 
   data(): State { 
     return {
       rates: null,
-      currencyToConvert: 'UAH',
-      convertedCurrency: 'USD',
-      currentRateOfCurrencyToConvert: 0,
-      currentRateOfConvertedCurrency : 0,
-      amountOfCurrencyToConvert: 0,
-      amountOfConvertedCurrency: 0,
-      conversionLimit: 10000
     };
   },
 
@@ -44,181 +32,14 @@ export default defineComponent({
       cachedExchangeRates = ratesFromServer;
     }
 
-    this.currentRateOfCurrencyToConvert = cachedExchangeRates[this.currencyToConvert]
-    this.currentRateOfConvertedCurrency = cachedExchangeRates[this.convertedCurrency]
-
     this.rates = cachedExchangeRates
   },
-
-  watch: {
-    currencyToConvert() {
-      if (this.rates) {
-        this.currentRateOfCurrencyToConvert = +this.rates[this.currencyToConvert]
-        this.amountOfConvertedCurrency = +this.newAmountConvertedCurrency;
-
-        if (this.isLimitReached) {
-          this.calculateTheBiggestAmount();
-        }
-      }
-    },
-
-    convertedCurrency() {
-      if (this.rates) {
-        this.currentRateOfConvertedCurrency = +this.rates[this.convertedCurrency]
-        this.amountOfConvertedCurrency = +this.newAmountConvertedCurrency
-
-        if (this.isLimitReached) {
-          this.calculateTheBiggestAmount();
-        }
-      }
-    },
-
-    amountOfCurrencyToConvert() {
-      if (!this.rates){
-        return;
-      }
-      
-      if (this.isLimitReached) {
-        this.calculateTheBiggestAmount();
-
-        return;
-      }
-
-      if (this.$refs['outcoming__currency'] !== document.activeElement) {
-        this.amountOfConvertedCurrency = this.newAmountConvertedCurrency 
-      }
-    },
-
-    amountOfConvertedCurrency() {
-      if (this.$refs['incoming__currency'] !== document.activeElement) { 
-        this.amountOfCurrencyToConvert = this.newAmountOfConvertedCurrency
-      }
-    },
-  },
-
-  computed: {
-    currenciesArr() {
-      const allCurrenciesNames: string[] = [];
-
-      for (const nameOfCurrency in this.rates) {
-        allCurrenciesNames.push(nameOfCurrency)
-      }
-
-      return allCurrenciesNames.sort((a, b) => a.localeCompare(b));
-    },
-
-    newAmountConvertedCurrency() {
-      return this.amountOfCurrencyToConvert
-        / this.currentRateOfCurrencyToConvert
-        * this.currentRateOfConvertedCurrency
-    },
-
-    newAmountOfConvertedCurrency() {
-      return this.amountOfConvertedCurrency
-        / (this.currentRateOfConvertedCurrency / this.currentRateOfCurrencyToConvert)
-    },
-
-    isLimitReached() {
-      if (this.rates) {
-        return this.amountOfCurrencyToConvert
-          / +this.rates[this.currencyToConvert] > this.conversionLimit;
-      }
-
-      return false;
-    }
-  },
-
-  methods: {
-    calculateTheBiggestAmount() {
-      if (this.rates) {
-        this.amountOfCurrencyToConvert = +(this.conversionLimit * +this.rates[this.currencyToConvert])
-          .toFixed(2);
-
-        this.amountOfConvertedCurrency = +(this.conversionLimit * +this.rates[this.convertedCurrency])
-          .toFixed(2);
-      }
-    },
-  }
 });
 
 </script>
 
 <template>
-  <h1 class="title">Currency converter</h1>
-  <form class="form">
-    <label for="from_currency" class="form__label">currency to conversion:</label>
-    <select 
-      id="from_currency" 
-      name="from_currency"
-      class="form__selector"
-      v-model="currencyToConvert"
-    >
-      <option 
-        v-for="name in currenciesArr" 
-        :key="name"
-        :value="name"
-        :selected="name === 'USD'"
-      >
-      {{ name }}
-      </option>
-    </select>
+  <CurrencyConverter :rates="rates" v-if="rates"/>
 
-    <label for="to_currency" class="form__label">converted currency:</label>
-    <select 
-      id="to_currency" 
-      name="to_currency" 
-      class="form__selector"
-      v-model="convertedCurrency"
-
-    >
-      <option 
-        v-for="name in currenciesArr" 
-        :key="name"
-        :value="name"
-        :selected="name === 'BTC'"
-      >
-      {{ name }}
-      </option>
-    </select>
-
-    <label 
-      for="from_amount"
-      class="form__label"
-    >
-      Amount of incoming currency:
-    </label>
-
-    <input 
-      ref="incoming__currency"
-      type="number" 
-      class="form__input"
-      id="from_amount" 
-      name="from_amount" 
-      min="0"
-      step="0.1"
-      v-model="amountOfCurrencyToConvert"
-      required
-    >
-
-    <label 
-      for="to_amount"
-      class="form__label"
-    >
-      Amount of converted currency:
-    </label>
-
-    <input 
-      ref="outcoming__currency"
-      type="number"
-      class="form__input"
-      id="to_amount"
-      name="to_amount"
-      min="0"
-      step="0.01"
-      v-model="amountOfConvertedCurrency"
-      required
-    >
-  </form>
-
-  <CurrencyRates git />
+  <CurrencyRates :rates="rates"/>
 </template>
--- src/components/Currency-rates.vue
+++ src/components/Currency-rates.vue
@@ -1,8 +1,48 @@
 <script lang="ts">
-import { defineComponent } from 'vue';
+import type { Rates } from '@/types/ratesType';
+import { defineComponent, type PropType } from 'vue';
+
+interface State {
+  selectedCurrency: string,
+  defaultCurrencies: string[];
+  defaultCurrenciesRates: string[];
+}
 
 export default defineComponent({
+  props: {
+    rates: {
+      type: Object as PropType<Rates | null>,
+      required: true,
+    }
+  },
 
+  data(): State {
+    return {
+      selectedCurrency: 'USD',
+      defaultCurrencies: ['USD', 'EUR', 'UAH'],
+      defaultCurrenciesRates: ['USD', 'EUR', 'UAH', 'BTC', 'ETH']
+    };
+  },
+
+  methods: {
+    getConvertedPrice(price: number) {
+      if (this.rates) {
+        return 1 / (price / +this.rates[this.selectedCurrency]);
+      }
+    }
+  },
+
+  computed: {
+    currenciesNamesArr() {
+      const allCurrenciesNames: string[] = [];
+
+      for (const nameOfCurrency in this.rates) {
+        allCurrenciesNames.push(nameOfCurrency)
+      }
+
+      return allCurrenciesNames.sort((a, b) => a.localeCompare(b));
+    },
+  },
 })
 </script>
 
@@ -11,24 +51,31 @@ export default defineComponent({
 
   <form class="form">
 
-    <label for="from_currency" class="form__label">currency to conversion:</label>
-    <select id="from_currency" name="from_currency" class="form__selector">
-      <option>123123</option>
+    <label for="from_currency" class="form__label">
+      currency:
+    </label>
+
+    <select v-model="selectedCurrency" id="from_currency" name="from_currency" class="form__selector">
+      <option v-for="name in defaultCurrencies" :key="name" :selected="name === selectedCurrency">
+        {{ name }}
+      </option>
     </select>
 
     <table class="table">
       <tr class="table__row">
-        <th class="table__head">Колонка 1</th>
-        <th class="table__head">Колонка 2</th>
+        <th class="table__head">Currency Name</th>
+        <th class="table__head">The rate in the selected currency</th>
       </tr>
-      <tr class="table__row">
-        <td class="table__colum">Рядок 1, Колонка 1</td>
-        <td class="table__colum">Рядок 1, Колонка 2</td>
-      </tr>
-      <tr class="table__row">
-        <td class="table__colum">Рядок 2, Колонка 1</td>
-        <td class="table__colum">Рядок 2, Колонка 2</td>
-      </tr>
+
+      <template 
+        v-for="currency in defaultCurrenciesRates" 
+        :key="currency"
+      >
+        <tr class="table__row" v-if="currency !== selectedCurrency">
+          <td class="table__colum">{{ currency }}</td>
+          <td class="table__colum">{{ rates ? getConvertedPrice(+rates[currency]) : 'error' }}</td>
+        </tr>
+      </template>
     </table>
   </form>
 </template>
